fa10cf0c6553a638c11c292c6b7d23179525f93a
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-19 20:35:37 +0800

    Update rebootschedule.lua

--- feeds/luci/applications/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua
+++ feeds/luci/applications/luci-app-rebootschedule/luasrc/controller/rebootschedule.lua
@@ -2,7 +2,6 @@ module("luci.controller.rebootschedule", package.seeall)
 function index()
 	if not nixio.fs.access("/etc/config/rebootschedule") then
 		return
-	end
 	
 	entry({"admin", "system", "rebootschedule"}, template("rebootschedule/rebootschedule"), _("定时设置"), 10).leaf = true
 end
